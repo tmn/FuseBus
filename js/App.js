bab2dccbcc2c08caf9069e412c168c94699d13f7
Tri Nguyen <mail@trimn.net>
2015-10-12 06:08:32 +0200

    Initial commit

---
+++ js/App.js
@@ -0,0 +1,114 @@
+var DEV = true;
+
+var Bussholdeplass    = require('Bussholdeplass')
+, Departure           = require('Departure')
+, Observable          = require('FuseJS/Observable');
+
+var current_page      = Observable('home')
+, data                = Observable()
+, departures          = Observable()
+, filtered_view       = Observable()
+, search_placeholder  = Observable('Søk etter holdeplass')
+, stop_info           = Observable()
+, stop_search         = Observable('');
+
+
+/* Mock data
+-----------------------------------------------------------------------------*/
+if (DEV) {
+  departures.add(new Departure('4', '10.10.2015 21:38', '10.10.2015 21:33', 1, 'Munkegata M5'));
+  departures.add(new Departure('6', '10.10.2015 21:58', '10.10.2015 21:58', 0, 'Værestrøa'));
+  departures.add(new Departure('38', '10.10.2015 22:01', '10.10.2015 21:44', 1, 'Stjørdal st.'));
+  departures.add(new Departure('46', '10.10.2015 22:02', '10.10.2015 22:02', 0, 'Pirbadet'));
+  departures.add(new Departure('4', '10.10.2015 22:03', '10.10.2015 22:03', 0, 'Lade'));
+  departures.add(new Departure('6', '10.10.2015 22:28', '10.10.2015 22:28', 0, 'Værestrøa'));
+  departures.add(new Departure('4', '10.10.2015 21:38', '10.10.2015 21:33', 1, 'Munkegata M5'));
+  departures.add(new Departure('6', '10.10.2015 21:58', '10.10.2015 21:58', 0, 'Værestrøa'));
+  departures.add(new Departure('38', '10.10.2015 22:01', '10.10.2015 21:44', 1, 'Stjørdal st.'));
+  departures.add(new Departure('46', '10.10.2015 22:02', '10.10.2015 22:02', 0, 'Pirbadet'));
+  departures.add(new Departure('4', '10.10.2015 22:03', '10.10.2015 22:03', 0, 'Lade'));
+  departures.add(new Departure('6', '10.10.2015 22:28', '10.10.2015 22:28', 0, 'Værestrøa'));
+
+  filtered_view.add(new Bussholdeplass('100268', '16011333', 'Gløshaugen Nord', 10.406111, 63.418309));
+  filtered_view.add(new Bussholdeplass('100295', '16010333', 'Gløshaugen Nord', 10.405967, 63.418184));
+  filtered_view.add(new Bussholdeplass('102714', '16011265', 'Gløshaugen Syd', 10.406111, 63.418309));
+}
+
+
+/* Func
+-----------------------------------------------------------------------------*/
+var go_back = function () {
+  current_page.value = 'home';
+};
+
+var stop_clicked = function (args) {
+  stop_info.clear();
+  stop_info.value = args.data;
+  stop_info.value.name = stop_info.value.name.toUpperCase();
+
+  fetch('http://bybussen.api.tmn.io/rt/' + args.data.locationId)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (responseObject) {
+    departures.clear();
+
+    responseObject['next'].map(function (e) {
+      departures.add(new Departure(e.l, e.t, e.ts, e.rt, e.d));
+    });
+
+    current_page.value = 'departures';
+  });
+}
+
+
+/* Data fetching
+-----------------------------------------------------------------------------*/
+stop_search.addSubscriber(function () {  
+  if (stop_search.value.length < 3) {
+    return;
+  }
+
+  filtered_view.clear();
+
+  data.forEach(function (e) {
+    if (e.name.toUpperCase().indexOf(stop_search.value.toUpperCase()) > -1) {
+      filtered_view.add(e);
+    }
+  });
+});
+
+stop_search.addSubscriber(function () {
+  if (stop_search.value.length > 0) {
+    search_placeholder.value = '';
+  }
+  else {
+    search_placeholder.value = 'Søk etter holdeplass';
+  }
+});
+
+fetch('http://bybussen.api.tmn.io/stops')
+.then(function (response) { 
+  return response.json();
+})
+.then(function (responseObject) { 
+  debug_log('heihei')
+  responseObject.map(function (e) {
+    data.add(new Bussholdeplass(e.busStopId, e.locationId, e.name, e.longitude, e.latitude));
+  });
+});
+
+
+/* Exports
+-----------------------------------------------------------------------------*/
+module.exports = {
+  current_page: current_page,
+  data: data,
+  departures: departures,
+  filtered_view: filtered_view,
+  go_back: go_back,
+  search_placeholder: search_placeholder,
+  stop_clicked: stop_clicked,
+  stop_info: stop_info,
+  stop_search: stop_search
+};
---
+++ js/Bussholdeplass.js
@@ -0,0 +1,39 @@
+var get_direction = function (locationId) {
+  if (locationId.length == 8) {
+    switch (parseInt(get_direction_code(locationId))) {
+      case 1:  return 'til midtbyen';
+      case 0:  return 'fra midtbyen';
+      case 20: return 'mot Stjørdal';
+      case 83: return 'mot Trondheim';
+      case 6:  return 'mot Klæbu';
+      case 7:  return 'mot Klæbu';
+      case 40: return 'vestover';
+      case 87: return 'østover';
+      default: return '...';
+    }
+  }
+};
+
+var get_direction_code = function (locationId) {
+  var directionCode = parseInt(locationId.substring(4, 5));
+  if (directionCode === 1 || directionCode === 0 || directionCode === 6 || directionCode === 7) {
+    return directionCode;
+  }
+
+  directionCode = parseInt(locationId.substring(4, 6));
+  if (directionCode === 20 || directionCode === 83 || directionCode === 40 || directionCode === 87) {
+    return directionCode;
+  }
+};
+
+
+/* ...
+-----------------------------------------------------------------------------*/
+module.exports = function (id, locationId, name, lon, lat) {
+  this.id = id;
+  this.locationId = locationId;
+  this.name = name;
+  this.lon = lon;
+  this.lat = lat;
+  this.direction = get_direction(locationId);
+}
