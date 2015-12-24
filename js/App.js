cc7e4700fe71ac67938ec1afeef8eecf57c4c870
tmn <mail@trimn.net>
2015-12-24 03:58:57 +0100

    Add initial work on geolocation

--- js/App.js
+++ js/App.js
@@ -1,6 +1,7 @@
 var Bussholdeplass    = require('Bussholdeplass')
 , Departure           = require('Departure')
 , Observable          = require('FuseJS/Observable')
+, GeoLocation         = require('FuseJS/GeoLocation')
 , Stops               = require('Stops');
 
 var departures        = Observable()
@@ -8,6 +9,7 @@ var departures        = Observable()
 , filtered_view       = Observable()
 , loading_indicator   = Observable(false)
 , departures_active   = Observable(false)
+, nearest_stops       = Observable()
 , stop_info           = Observable()
 , stop_search         = Observable('');
 
@@ -59,19 +61,67 @@ var stop_clicked = function (args) {
 };
 
 
+/* Geolocation + nearest stops
+-----------------------------------------------------------------------------*/
+GeoLocation.onChanged = function (location) {
+  console.log('CHANGED');
+  updateNearestStops(location);  
+}
+
+if (GeoLocation.location !== null) {
+  updateNearestStops(GeoLocation.location);
+}
+
+updateNearestStops();
+
+var minimumReportInterval = 1000, desiredAccuracyInMeters = 10;
+GeoLocation.startListening(minimumReportInterval, desiredAccuracyInMeters);
+
+function stop() {
+   GeoLocation.stopListening();
+}
+
+function updateNearestStops(location) {
+  if (location === undefined) {
+    if (GeoLocation.location !== null) {
+      location = GeoLocation.location;
+    }
+    else {
+      return;
+    }
+  }
+
+  fetch('http://bybussen.api.tmn.io/stops/nearest/' + location.latitude + '/' + location.longitude)
+  .then(function (response) {
+    console.log('asdf');
+    return response.json();
+  })
+  .then(function (responseObject) {
+    var convertedStops = responseObject.map(function (e) {
+      return new Bussholdeplass(e.busStopId, e.locationId, e.name, e.longitude, e.latitude, e.distance);
+    });
+
+    filtered_view.replaceAll(convertedStops);
+  })
+  .catch(function (err) {
+    console.log('ERROR: ' + err.message);
+  });
+}
+
+
 /* Data fetching
 -----------------------------------------------------------------------------*/
 stop_search.addSubscriber(function () {
   if (stop_search.value.length < 3) {
     if (stop_search.value.length === 0) {
       filtered_view.clear();
+      updateNearestStops();
     }
 
     return;
   }
 
   var search_string = stop_search.value.toUpperCase();
-
   filtered_view.replaceAll(Stops.filter(function (e) {
     return e.name.toUpperCase().indexOf(search_string) > -1;
   }));
