fe990a1d1369d71771e0bd83a48ddc79c805679a
Tri Nguyen <mail@trimn.net>
2016-05-07 14:47:48 +0200

    Calculate timeleft by seconds instead of minutes

--- js/Components/Departure.js
+++ js/Components/Departure.js
@@ -2,14 +2,17 @@ var calculate_timeleft = function (realtime, orgtime, rt) {
   var tmp_time      = rt ? realtime.split(/[\.: ]/) : orgtime.split(/[\.: ]/)
   , currentDateTime = new Date()
   , departureTime   = new Date(tmp_time[2], tmp_time[1]-1, tmp_time[0], tmp_time[3], tmp_time[4], 0)
-  , diffMinutes = Math.round(Math.abs((departureTime.getTime() - currentDateTime.getTime()) / (60 * 1000)));
+  , diffSeconds = Math.round(Math.abs((departureTime.getTime() - currentDateTime.getTime()) / (1000)));
 
-  if (diffMinutes > 60) {
-    var timestamp = ('00' + departureTime.getHours()).slice()-2 + ':' + ('00' + departureTime.getMinutes()).slice(-2);
+  if (diffSeconds > 3600) {
+    var timestamp = ('00' + departureTime.getHours()).slice(-2) + ':' + ('00' + departureTime.getMinutes()).slice(-2);
     return rt ? timestamp : 'ca ' + timestamp;
   }
+  else if (diffSeconds < 60) {
+    return rt ? 'Nå' : 'ca ' + 'Nå';
+  }
 
-  return rt ? diffMinutes + ' min' : 'ca ' + diffMinutes + ' min';
+  return rt ? Math.ceil((diffSeconds/60)) + ' min' : 'ca ' + Math.ceil((diffSeconds/60)) + ' min';
 }
 
 
@@ -20,6 +23,7 @@ module.exports = function (line, realtime, orgtime, is_realtime, destination) {
   this.orgtime = orgtime.split(' ')[1];
   this.is_realtime = is_realtime;
   this.destination = destination;
+  this.timeDiff = realtime !== orgtime;
 
   this.timeLeft = calculate_timeleft(realtime, orgtime, is_realtime);
 };
