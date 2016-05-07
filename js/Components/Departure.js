17cc2ef220edd84d3495717c87758f071bef3376
Tri Nguyen <mail@trimn.net>
2016-05-07 14:17:50 +0200

    Update timeleft calculcation

--- js/Components/Departure.js
+++ js/Components/Departure.js
@@ -1,37 +1,25 @@
-var calculate_timeleft = function (time, rt) {
-  var tmp_time      = time.split(/[\.: ]/)
+var calculate_timeleft = function (realtime, orgtime, rt) {
+  var tmp_time      = rt ? realtime.split(/[\.: ]/) : orgtime.split(/[\.: ]/)
   , currentDateTime = new Date()
-  , departureTime   = new Date(tmp_time[2], tmp_time[1]-1, tmp_time[0], tmp_time[3], tmp_time[4], 0);
+  , departureTime   = new Date(tmp_time[2], tmp_time[1]-1, tmp_time[0], tmp_time[3], tmp_time[4], 0)
+  , diffMinutes = Math.round(Math.abs((departureTime.getTime() - currentDateTime.getTime()) / (60 * 1000)));
 
-  if (currentDateTime.getFullYear() != departureTime.getFullYear()) {
-    return '...';
+  if (diffMinutes > 60) {
+    var timestamp = ('00' + departureTime.getHours()).slice()-2 + ':' + ('00' + departureTime.getMinutes()).slice(-2);
+    return rt ? timestamp : 'ca ' + timestamp;
   }
 
-  var departureTimeMinutes = departureTime.getHours()*60 + departureTime.getMinutes()
-  , currentDateTimeMinutes = currentDateTime.getHours()*60 + currentDateTime.getMinutes()
-  , interval               = departureTimeMinutes - currentDateTimeMinutes;
+  return rt ? diffMinutes + ' min' : 'ca ' + diffMinutes + ' min';
+}
 
-  if (currentDateTime.getMonth() == departureTime.getMonth() && currentDateTime.getDate() == departureTime.getDate()) {
-    if (interval < 60) {
-      if (interval < 1) {
-        return rt ? 'Nå' : 'ca nå';
-      }
-      else {
-        return rt ? interval + ' min' : 'ca ' + interval + ' min';
-      }
-    }
-  }
 
-  return departureTime.getHours() + ':' +(departureTime.getMinutes()< 10 ? '0' : '' ) + departureTime.getMinutes();
-};
-
-module.exports = function (line, time, realtime, is_realtime, destination) {
+module.exports = function (line, realtime, orgtime, is_realtime, destination) {
   this.line = line;
-  this.time = time;
-  this.realtime = realtime;
+  this.time = orgtime;
+  this.realtime = realtime.split(' ')[1];
+  this.orgtime = orgtime.split(' ')[1];
   this.is_realtime = is_realtime;
   this.destination = destination;
-  this.org_time = time.split(' ')[1];
 
-  this.timeLeft = calculate_timeleft(time, realtime, is_realtime);
+  this.timeLeft = calculate_timeleft(realtime, orgtime, is_realtime);
 };
